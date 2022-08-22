481ed760c3015d405ac13769888bd6edcf65e3be
Sergii <mysnianko@gmail.com>
2022-08-22 12:46:55 +0100

    Add a files

---
+++ jsindex.js
@@ -0,0 +1,10 @@
+alert("Hi all! This is the first homework. And the first mission! We need to create a script to add, subtract, multiply and divide 2 numbers. For interest, we will ask you to enter these numbers. Don't be sad, it won't be long!");
+let firstInput = prompt("Could you please enter the first number?");
+let secondInput = prompt("Could you please enter the second number?");
+let firstNumber = parseInt(firstInput);
+let secondNumber = parseInt(secondInput);
+let addResult = firstNumber + secondNumber;
+let subtractResult = firstNumber - secondNumber;
+let multiplyResult = firstNumber * secondNumber;
+let divideResult = firstNumber / secondNumber;
+alert("Your numbers: " + firstNumber + " and " + secondNumber + "." + " Addition: " + addResult + "." + " Difference: " + subtractResult + "." + " Product:  " + multiplyResult + "." + " Quotient: " + divideResult + "." + " Well, how? Did you like the game? Try again!");
